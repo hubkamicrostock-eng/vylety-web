e84f15fa80135ba0ed7c94ff8e3730bba17b1eba
hubkamicrostock-eng <hubka.microstock@gmail.com>
2026-05-05 17:08:42 +0200

    Switch to CartoDB Voyager tiles for better map rendering

--- map.js
+++ map.js
@@ -100,8 +100,8 @@ async function init() {
   }
 
   const map = L.map('map');
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a>',
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a> © <a href="https://carto.com">CARTO</a>',
     maxZoom: 19,
   }).addTo(map);
 
